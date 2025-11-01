649b64c8bd9c5120a235a51e2d2ac110eb2de532
santiagormrz <santiago.emanuelr@gmail.com>
2025-11-01 01:30:58 -0300

    ejercicio 6 y 7

--- servicio_clientes.py
+++ servicio_clientes.py
@@ -1,5 +1,32 @@
+import servicio_concesionarias
+
 class ServicioClientes:
 
     def obtener_total_ventas_por_cliente(self, concesionaria_id, cliente_id):
         
-        pass
+        servicio_conc = servicio_concesionarias.ServicioConcesionarias()
+        concesionaria = servicio_conc.obtener_por_id(concesionaria_id)
+
+        if concesionaria is None:
+            return 0
+        
+        cliente_existe = False
+
+        for cliente in concesionaria.obtener_clientes():
+            if cliente.obtener_numero_id() == int(cliente_id):
+                cliente_existe = True
+                break
+
+        if not cliente_existe:
+            return 0
+        
+        total_ventas = 0
+
+        for sucursal in concesionaria.obtener_sucursales():
+            for venta in sucursal.obtener_ventas():
+                if venta.obtener_cliente_id() == int(cliente_id):
+                    total_ventas += venta.obtener_monto()
+
+        return total_ventas
+
+
