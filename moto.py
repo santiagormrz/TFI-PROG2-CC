41b38764e48bccde2d969cb15835b8780a4b58a6
Emily Lange <emyyylange@gmail.com>
2025-11-02 01:42:32 -0300

    agrego método __str__ en moto

--- moto.py
+++ moto.py
@@ -14,3 +14,23 @@ class Moto(vehiculo.Vehiculo):
 
     def obtener_cilindrada(self):
         return self.__cilindrada
+    
+    def __str__(self):
+        return (
+            "Moto ID: "
+            + str(self.obtener_numero_id())
+            + " - "
+            + self.obtener_marca()
+            + " "
+            + self.obtener_modelo()
+            + " ("
+            + str(self.obtener_anio())
+            + ") "
+            + "\nSucursal ID: "
+            + str(self.obtener_sucursal_id())
+            + " - Estado ID: "
+            + str(self.obtener_estado_id())
+            + " - Cilindrada: "
+            + str(self.__cilindrada)
+            + "cc"
+        )
